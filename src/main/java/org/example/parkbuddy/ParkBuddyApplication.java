b414ecb2ee1c4e114f80e7b698d310f4643f4aeb
stamas02-hun <sirbiktamas2001@gmail.com>
2026-08-03 23:43:51 +0200

    Initial commit

---
+++ src/main/java/org/example/parkbuddy/ParkBuddyApplication.java
@@ -0,0 +1,13 @@
+package org.example.parkbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/parkbuddy/ParkBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.parkbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
