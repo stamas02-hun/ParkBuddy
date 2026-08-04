2e5deccec5122954be8916e5e6a57eb0f7d5993f
stamas02-hun <sirbiktamas2001@gmail.com>
2026-08-04 16:49:21 +0200

    Parking space service interface
    ParkingSpaceService.java:
     - Added service interface for parking spaces

---
+++ src/main/java/org/example/parkbuddy/service/ParkingSpaceService.java
@@ -0,0 +1,19 @@
+package org.example.parkbuddy.service;
+
+import org.example.parkbuddy.model.ParkingSpace;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ParkingSpaceService {
+
+    List<ParkingSpace> getParkingSpaces();
+
+    Optional<ParkingSpace> getParkingSpaceById(long id);
+
+    ParkingSpace createParkingSpace(ParkingSpace parkingSpace);
+
+    ParkingSpace modifyParkingSpace(ParkingSpace parkingSpace);
+
+    void deleteParkingSpace(ParkingSpace parkingSpace);
+}
